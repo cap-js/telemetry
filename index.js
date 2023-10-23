f026594e809d28877918a6acfec8c0112104c7f4
D050513 <sebastian.van.syckel@sap.com>
2023-10-23 23:08:05 +0200

    formatting

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http')
 module.exports = {
   instrumentations: [
     new HttpInstrumentation({ ignoreIncomingPaths: cds.env.requires.otel.trace.ignorePaths }), //> REVISIT: why not use config name of third party?
-    new ExpressInstrumentation({ ignoreLayersType: cds.env.requires.otel.trace.ignoreExpressLayer }), //> REVISIT: why not use config name of third party?
+    new ExpressInstrumentation({ ignoreLayersType: cds.env.requires.otel.trace.ignoreExpressLayer }) //> REVISIT: why not use config name of third party?
   ]
 }
--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ process.env.CDS_ENV = 'better-sqlite'
 
 const config = {
   testTimeout: 5000,
-  testMatch: ['**/*.test.js'],
+  testMatch: ['**/*.test.js']
 }
 
 module.exports = config
