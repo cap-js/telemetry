8a6f1ca9b9f0b71000ae59c524c3296f426d1ae7
D050513 <sebastian.van.syckel@sap.com>
2023-10-31 16:15:33 +0100

    better test setup

--- test/tracing.test.js
+++ test/tracing.test.js
@@ -1,28 +1,32 @@
 const cds = require('@sap/cds')
-const bookshop = require('path').resolve(__dirname, './bookshop')
+const { expect, GET, POST } = cds.test().in(__dirname + '/bookshop')
+const log = cds.test.log()
 
 describe('Integration tests cds with open telemetry', () => {
-  const { expect, GET, POST } = cds.test().in(bookshop)
-  const admin = {
-    auth: {
-      username: 'alice'
-    }
-  }
-  test('GET request is traced', async () => {
-    const { status } = await GET('/admin/Genres', admin)
-    expect(status).to.equal(200)
-    // expect console to have trace logs
+  const admin = { auth: { username: 'alice' } }
+
+  beforeEach(() => {
+    log.clear()
   })
+
+  test('GET request is traced', async () => {
+    const { status } = await GET('/odata/v4/admin/Books', admin)
+    expect(status).to.equal(200)
+    // primitive check that console has trace logs
+    expect(log.output).to.match(/\[otel\] - \s+\d+\.\d+ ms/)
+  })
+
+  // --- HERE ---
 
   test.skip('cds.spawn is traced', async () => {})
 
-  test('srv.emit is traced', async () => {
+  test.skip('srv.emit is traced', async () => {
     const { status } = await POST('/browse/submitOrder', { book: 1, quantity: 1 }, admin)
     expect(status).to.equal(200)
     // Wait and afterwards check if trace of emit is part of exporter
   })
 
-  test('$batch is traced', async () => {
+  test.skip('$batch is traced', async () => {
     const CRLF = '\r\n'
     const batchBody = [
       '--batch_1',
@@ -42,7 +46,7 @@ describe('Integration tests cds with open telemetry', () => {
       '--batch_1--',
       ''
     ].join(CRLF)
-    const { status } = await POST('/admin/$batch', batchBody, {
+    const { status } = await POST('/odata/v4/admin/$batch', batchBody, {
       auth: admin.auth,
       headers: {
         'content-type': 'multipart/mixed;boundary=batch_1',
