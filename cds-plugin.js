866c1fb8119147d54b56fbaaad78b50975a12d1b
Steffen Waldmann <steffen.waldmann@sap.com>
2024-05-16 15:12:12 +0200

    Don't require lib if run in CLI

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,4 +1,4 @@
-if (!process.env.NO_TELEMETRY || process.env.NO_TELEMETRY === 'false') require('./lib')()
-
 const cds = require('@sap/cds')
+if (!cds.cli && (!process.env.NO_TELEMETRY || process.env.NO_TELEMETRY === 'false')) require('./lib')()
+
 cds.add?.register('telemetry', require('./lib/add'))
