d6494126afbe0a84d5d5c7a89984bb929b86a1d3
D050513 <sebastian.van.syckel@sap.com>
2024-06-17 09:29:09 +0200

    fix

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,11 +1,12 @@
-const cds = require('@sap/cds')
+let _startup = true
 
-if (!(cds.cli?.command in { '': 1, serve: 1, run: 1 })) return
+const cds = require('@sap/cds')
+if (!(cds.cli?.command in { '': 1, serve: 1, run: 1 })) _startup = false
 
 // cds add XXX currently also has cli.command === ''
 const i = process.argv.indexOf('add')
-if (i > 1 && process.argv[i - 1].match(/cds(\.js)?$/)) return
+if (i > 1 && process.argv[i - 1].match(/cds(\.js)?$/)) _startup = false
 
-if (!!process.env.NO_TELEMETRY && process.env.NO_TELEMETRY !== 'false') return
+if (!!process.env.NO_TELEMETRY && process.env.NO_TELEMETRY !== 'false') _startup = false
 
-require('./lib')()
+if (_startup) require('./lib')()
