2903937d3f0836d3252ebf4511d04bc401b3b72e
Steffen Waldmann <steffen.waldmann@sap.com>
2024-06-26 18:36:31 +0200

    More commands

--- cds-plugin.js
+++ cds-plugin.js
@@ -3,7 +3,9 @@ let _startup = true
 const cds = require('@sap/cds')
 if (!(cds.cli?.command in { '': 1, serve: 1, run: 1 })) _startup = false
 
-cds.add?.register('telemetry', require('./lib/add'))
+cds.add?.register('telemetry', require('./lib/add/cloud-logging'))
+cds.add?.register('cloud-logging', require('./lib/add/cloud-logging'))
+cds.add?.register('dynatrace', require('./lib/add/dynatrace'))
 
 if (!!process.env.NO_TELEMETRY && process.env.NO_TELEMETRY !== 'false') _startup = false
 
