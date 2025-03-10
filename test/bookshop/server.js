64e6239f1831a2a1b7784736d51895590df2f83b
D050513 <sebastian.van.syckel@sap.com>
2025-03-10 22:25:43 +0100

    rm unnecessary cds.connect.to('db')

--- test/bookshop/server.js
+++ test/bookshop/server.js
@@ -2,7 +2,6 @@ const cds = require('@sap/cds')
 
 cds.on('bootstrap', app => {
   app.use('/custom/Books', async (req, res) => {
-    await cds.connect.to('db')
     const books = await SELECT.from('Books')
     res.json(books)
   })
