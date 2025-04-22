c74f0d3b510b4003274a686b6468358830aa0be3
sjvans <30337871+sjvans@users.noreply.github.com>
2025-04-22 15:48:31 +0200

    feat: check versions of @opentelemetry dependencies (#330)

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,12 +1,28 @@
-let _startup = true
+;(() => {
+  const cds = require('@sap/cds')
+  if (!(cds.cli?.command in { '': 1, serve: 1, run: 1 })) return
 
-const cds = require('@sap/cds')
-if (!(cds.cli?.command in { '': 1, serve: 1, run: 1 })) _startup = false
+  // cds add XXX currently also has cli.command === ''
+  const i = process.argv.indexOf('add')
+  if (i > 1 && process.argv[i - 1].match(/cds(\.js)?$/)) return
 
-// cds add XXX currently also has cli.command === ''
-const i = process.argv.indexOf('add')
-if (i > 1 && process.argv[i - 1].match(/cds(\.js)?$/)) _startup = false
+  if (!!process.env.NO_TELEMETRY && process.env.NO_TELEMETRY !== 'false') return
 
-if (!!process.env.NO_TELEMETRY && process.env.NO_TELEMETRY !== 'false') _startup = false
+  // check versions of @opentelemetry dependencies
+  const { dependencies } = require(require('path').join(cds.root, 'package'))
+  let violations = []
+  for (const each in dependencies) {
+    if (!each.match(/^@opentelemetry\//)) continue
+    const { version } = require(`${each}/package.json`)
+    const [major, minor] = version.split('.')
+    if (major >= 2 || minor >= 200) violations.push(`${each}@${version}`)
+  }
+  if (violations.length) {
+    const msg =
+      '@cap-js/telemetry does not yet support OpenTelemetry SDK 2.0 (^2 and ^0.200):' +
+      `\n  - ${violations.join('\n  - ')}\n`
+    throw new Error(msg)
+  }
 
-if (_startup) require('./lib')()
+  require('./lib')()
+})()
