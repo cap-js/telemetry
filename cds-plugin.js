1e840de708af177fe2e3dbdfdd543f73a8adbb33
D050513 <sebastian.van.syckel@sap.com>
2025-04-22 15:43:44 +0200

    rm `_startup`

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,16 +1,14 @@
-let _startup = true
+;(() => {
+  const cds = require('@sap/cds')
+  if (!(cds.cli?.command in { '': 1, serve: 1, run: 1 })) return
 
-const cds = require('@sap/cds')
-if (!(cds.cli?.command in { '': 1, serve: 1, run: 1 })) _startup = false
+  // cds add XXX currently also has cli.command === ''
+  const i = process.argv.indexOf('add')
+  if (i > 1 && process.argv[i - 1].match(/cds(\.js)?$/)) return
 
-// cds add XXX currently also has cli.command === ''
-const i = process.argv.indexOf('add')
-if (i > 1 && process.argv[i - 1].match(/cds(\.js)?$/)) _startup = false
+  if (!!process.env.NO_TELEMETRY && process.env.NO_TELEMETRY !== 'false') return
 
-if (!!process.env.NO_TELEMETRY && process.env.NO_TELEMETRY !== 'false') _startup = false
-
-// check versions of @opentelemetry dependencies
-if (_startup) {
+  // check versions of @opentelemetry dependencies
   const { dependencies } = require(require('path').join(cds.root, 'package'))
   let violations = []
   for (const each in dependencies) {
@@ -25,6 +23,6 @@ if (_startup) {
       `\n  - ${violations.join('\n  - ')}\n`
     throw new Error(msg)
   }
-}
 
-if (_startup) require('./lib')()
+  require('./lib')()
+})()
