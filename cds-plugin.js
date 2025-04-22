2c59175e3593447c66ec458fe0de81d0b97fcd9d
D050513 <sebastian.van.syckel@sap.com>
2025-04-22 14:46:39 +0200

    feat: check versions of @opentelemetry dependencies

--- cds-plugin.js
+++ cds-plugin.js
@@ -9,4 +9,22 @@ if (i > 1 && process.argv[i - 1].match(/cds(\.js)?$/)) _startup = false
 
 if (!!process.env.NO_TELEMETRY && process.env.NO_TELEMETRY !== 'false') _startup = false
 
+// check versions of @opentelemetry dependencies
+if (_startup) {
+  const { dependencies } = require(require('path').join(cds.root, 'package'))
+  let violations = []
+  for (const each in dependencies) {
+    if (!each.match(/^@opentelemetry\//)) continue
+    const { version } = require(`${each}/package.json`)
+    const [major, minor] = version.split('.')
+    if (major >= 2 || minor >= 200) violations.push(`${each}@${version}`)
+  }
+  if (violations.length) {
+    const msg =
+      '@cap-js/telemetry does not yet support OpenTelemetry SDK 2.0 (^2 and ^0.200):' +
+      `\n  - ${violations.join('\n  - ')}\n`
+    throw new Error(msg)
+  }
+}
+
 if (_startup) require('./lib')()
