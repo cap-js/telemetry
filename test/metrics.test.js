946107683a82f8357e70f5f8b03b73dea897b33e
I548646 <paul.erlenwein@sap.com>
2025-05-22 17:39:09 +0200

    test: move proxy service impl into test

--- test/metrics.test.js
+++ test/metrics.test.js
@@ -27,6 +27,25 @@ describe("metrics", () => {
   });
 
   describe("outbox", () => {
+    let totalInc = 0;
+    let totalOut = 0;
+
+    beforeAll(async () => {
+      const proxyService = await cds.connect.to("ProxyService");
+      const externalService = await cds.connect.to("ExternalService");
+      const outboxedService = cds.outboxed(externalService);
+
+      proxyService.on("proxyCallToExternalService", async (req) => {
+        await outboxedService.send("call", {});
+        totalInc += 1;
+        return req.reply("OK");
+      });
+
+      externalService.before("*", () => {
+        totalOut += 1;
+      });
+    });
+
     const metricValue = (metric) => {
       const regx = new RegExp(
         `outbox\\.${metric}[\\s\\S]*?value:\\s*(\\d+)`,
@@ -43,13 +62,13 @@ describe("metrics", () => {
 
       await wait(150); // Wait for metrics to be collected
 
-      expect(metricValue("cold_entries")).to.equal(0);
-      expect(metricValue("remaining_entries")).to.equal(0);
-      expect(metricValue("incoming_messages")).to.equal(1);
-      expect(metricValue("outgoing_messages")).to.equal(1);
-      expect(metricValue("min_storage_time_in_seconds")).to.equal(0);
-      expect(metricValue("med_storage_time_in_seconds")).to.equal(0);
-      expect(metricValue("max_storage_time_in_seconds")).to.equal(0);
+      expect(metricValue("cold_entries")).to.eq(0);
+      expect(metricValue("remaining_entries")).to.eq(0);
+      expect(metricValue("incoming_messages")).to.eq(totalInc);
+      expect(metricValue("outgoing_messages")).to.eq(totalOut);
+      expect(metricValue("min_storage_time_in_seconds")).to.eq(0);
+      expect(metricValue("med_storage_time_in_seconds")).to.eq(0);
+      expect(metricValue("max_storage_time_in_seconds")).to.eq(0);
     });
 
     describe("target service requires retries", () => {
@@ -78,45 +97,41 @@ describe("metrics", () => {
         await GET("/odata/v4/proxy/proxyCallToExternalService", admin);
 
         await wait(100); // Wait for metrics to be collected
-        expect(currentRetryCount).to.equal(1);
+        expect(currentRetryCount).to.eq(1);
 
-        expect(metricValue("cold_entries")).to.equal(0);
-        expect(metricValue("remaining_entries")).to.equal(1);
-        expect(metricValue("incoming_messages")).to.equal(1);
-        expect(metricValue("outgoing_messages")).to.equal(1);
-        expect(metricValue("min_storage_time_in_seconds")).to.equal(0);
-        expect(metricValue("med_storage_time_in_seconds")).to.equal(0);
-        expect(metricValue("max_storage_time_in_seconds")).to.equal(0);
+        expect(metricValue("cold_entries")).to.eq(0);
+        expect(metricValue("remaining_entries")).to.eq(1);
+        expect(metricValue("incoming_messages")).to.eq(totalInc);
+        expect(metricValue("outgoing_messages")).to.eq(totalOut);
+        expect(metricValue("min_storage_time_in_seconds")).to.eq(0);
+        expect(metricValue("med_storage_time_in_seconds")).to.eq(0);
+        expect(metricValue("max_storage_time_in_seconds")).to.eq(0);
 
         // Wait for the first retry to be initiated
         while (currentRetryCount < 2) await wait(250);
         await wait(200); // Wait for retry to be processed and metrics to be collected
+        expect(currentRetryCount).to.eq(2);
 
-        expect(metricValue("cold_entries")).to.equal(0);
-        expect(metricValue("remaining_entries")).to.equal(1);
-        expect(metricValue("incoming_messages")).to.equal(1);
-        expect(metricValue("outgoing_messages")).to.equal(2);
-        expect(
-          metricValue("min_storage_time_in_seconds")
-        ).to.be.greaterThanOrEqual(1);
-        expect(
-          metricValue("med_storage_time_in_seconds")
-        ).to.be.greaterThanOrEqual(1);
-        expect(
-          metricValue("max_storage_time_in_seconds")
-        ).to.be.greaterThanOrEqual(1);
+        expect(metricValue("cold_entries")).to.eq(0);
+        expect(metricValue("remaining_entries")).to.eq(1);
+        expect(metricValue("incoming_messages")).to.eq(totalInc);
+        expect(metricValue("outgoing_messages")).to.eq(totalOut);
+        expect(metricValue("min_storage_time_in_seconds")).to.be.gte(1);
+        expect(metricValue("med_storage_time_in_seconds")).to.be.gte(1);
+        expect(metricValue("max_storage_time_in_seconds")).to.be.gte(1);
 
         // Wait for the second retry to be initiated
         while (currentRetryCount < 3) await wait(250);
-        await wait(500); // Wait for retry to be processed and metrics to be collected
+        await wait(200); // Wait for retry to be processed and metrics to be collected
+        expect(currentRetryCount).to.eq(3);
 
-        expect(metricValue("cold_entries")).to.equal(0);
-        expect(metricValue("remaining_entries")).to.equal(0);
-        expect(metricValue("incoming_messages")).to.equal(1);
-        expect(metricValue("outgoing_messages")).to.equal(3);
-        expect(metricValue("min_storage_time_in_seconds")).to.equal(0);
-        expect(metricValue("med_storage_time_in_seconds")).to.equal(0);
-        expect(metricValue("max_storage_time_in_seconds")).to.equal(0);
+        expect(metricValue("cold_entries")).to.eq(0);
+        expect(metricValue("remaining_entries")).to.eq(0);
+        expect(metricValue("incoming_messages")).to.eq(totalInc);
+        expect(metricValue("outgoing_messages")).to.eq(totalOut);
+        expect(metricValue("min_storage_time_in_seconds")).to.eq(0);
+        expect(metricValue("med_storage_time_in_seconds")).to.eq(0);
+        expect(metricValue("max_storage_time_in_seconds")).to.eq(0);
       });
     });
   });
