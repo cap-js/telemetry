15ed2ddf519333928e7262257d38ec3f671d7cc7
D050513 <sebastian.van.syckel@sap.com>
2025-06-04 17:16:41 +0200

    outbox: { kind: 'in-memory-outbox' }

--- test/tracing-messaging-with-in-memory-outbox.test.js
+++ test/tracing-messaging-with-in-memory-outbox.test.js
@@ -2,16 +2,14 @@ const CASE = 'with_in_memory_outbox'
 
 const env = {
   kind: 'file-based-messaging',
-  outbox: true,
+  outbox: { kind: 'in-memory-outbox' },
   file: `../${CASE}`
 }
 process.env.cds_requires_messaging = JSON.stringify(env)
-process.env.cds_requires_outbox = JSON.stringify({ kind: 'in-memory-outbox' })
 
 const CHECK = (log, expect) => {
   // 2: no outbox -> consumer gets new root context
   // REVISIT: for some reason, the emit done in the on succeeded callback gets a new root context when running in jest
-  expect(log.output).to.equal('dummy')
   expect(log.output.match(/\[telemetry\] - elapsed times:/g).length).to.equal(3) //> actually 2
 }
 
