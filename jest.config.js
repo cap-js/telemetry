40f1eeb27a08b8b7a650bb45b4dcc9e1ade6bf39
D050513 <sebastian.van.syckel@sap.com>
2025-06-26 22:55:18 +0200

    hana workflow

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,9 @@ const config = {
   testMatch: ['**/*.test.js']
 }
 
+if (process.env.HANA_DRIVER) {
+  config.testTimeout *= 10
+  // config.testMatch = ['**/tracing-attributes.test.js']
+}
+
 module.exports = config
