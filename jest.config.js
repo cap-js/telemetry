f056d11b3f52287d080d3327f7ef563e0c11a61c
D050513 <sebastian.van.syckel@sap.com>
2025-06-26 23:12:02 +0200

    add _hana_prom to matrix

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,9 @@ if (process.env.HANA_DRIVER) {
 
   const credentials = JSON.parse(process.env.CAPJS_TELEMETRY_TEST_BOOKSHOP_DB_CREDENTIALS)
   process.env.cds_requires_db = JSON.stringify({ kind: 'hana', credentials })
+
+  if (process.env.HANA_PROM)
+    process.env.cds_requires_telemetry_tracing = JSON.stringify({ _hana_prom: process.env.HANA_PROM === 'true' })
 }
 
 module.exports = config
