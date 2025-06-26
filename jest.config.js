10893e6ccdc53ab5563768403d462e3f511e3a4c
D050513 <sebastian.van.syckel@sap.com>
2025-06-26 23:53:56 +0200

    HANA_CREDS

--- jest.config.js
+++ jest.config.js
@@ -7,8 +7,7 @@ if (process.env.HANA_DRIVER) {
   config.testTimeout *= 10
   config.testMatch = ['**/tracing-attributes.test.js']
 
-  const credentials = JSON.parse(process.env.CAPJS_TELEMETRY_TEST_BOOKSHOP_DB_CREDENTIALS)
-  process.env.cds_requires_db = JSON.stringify({ kind: 'hana', credentials })
+  process.env.cds_requires_db = JSON.stringify({ kind: 'hana', credentials: JSON.parse(process.env.HANA_CREDS) })
 
   if (process.env.HANA_PROM)
     process.env.cds_requires_telemetry_tracing = JSON.stringify({ _hana_prom: process.env.HANA_PROM === 'true' })
