07d08d4b5a0f202e39a400262b82a6f94b5a1183
D050513 <sebastian.van.syckel@sap.com>
2025-06-26 23:06:18 +0200

    CAPJS_TELEMETRY_TEST_BOOKSHOP_DB_CREDENTIALS

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,9 @@ const config = {
 if (process.env.HANA_DRIVER) {
   config.testTimeout *= 10
   // config.testMatch = ['**/tracing-attributes.test.js']
+
+  const credentials = JSON.parse(process.env.CAPJS_TELEMETRY_TEST_BOOKSHOP_DB_CREDENTIALS)
+  process.env.cds_requires_db = JSON.stringify({ kind: 'hana', credentials })
 }
 
 module.exports = config
