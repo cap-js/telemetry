11eafefb60b53a4fc5c47100db5e1a1d508e9e27
D050513 <sebastian.van.syckel@sap.com>
2025-07-08 15:51:46 +0200

    own hdi container

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ if (process.env.CI && process.env.HANA_DRIVER) {
   config.testTimeout *= 10
   config.testMatch = ['**/tracing-attributes.test.js']
 
-  process.env.cds_requires_db = JSON.stringify({ kind: 'hana', credentials: JSON.parse(process.env.HANA_CREDS) })
+  // process.env.cds_requires_db = JSON.stringify({ kind: 'hana', credentials: JSON.parse(process.env.HANA_CREDS) })
 
   if (process.env.HANA_PROM)
     process.env.cds_requires_telemetry_tracing = JSON.stringify({ _hana_prom: process.env.HANA_PROM === 'true' })
