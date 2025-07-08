570031fc9c7ce02bf5bc5065f061c5aa46bf7995
Paul <paul.erlenwein@gmail.com>
2025-07-08 12:06:19 +0200

    feat: adopt stable attributes (#354)
    Resolves https://github.com/cap-js/telemetry/issues/337
    
    ---------
    
    Co-authored-by: sjvans <30337871+sjvans@users.noreply.github.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org/).
 The format is based on [Keep a Changelog](http://keepachangelog.com/).
 
+## Version 1.5.0 - 2025-07-08
+
+### Added
+
+- Adopt stable semantic conventions
+
 ## Version 1.4.1 - 2025-06-27
 
 ### Fixed
--- test/_db_spans.js
+++ test/_db_spans.js
@@ -4,14 +4,14 @@ module.exports = {
       // name: "@cap-js/sqlite - prepare SELECT json_insert('{}','$.\"createdAt\"',createdAt,'$.\"…",
       name: s => s.match(/@cap-js\/\w+ - prepare /),
       attributes: {
-        'code.function': 'prepare',
-        'db.system': s => s === 'sqlite' || 'hanadb',
-        'db.name': 'db',
+        'code.function.name': 'prepare',
+        'db.system.name': s => s === 'sqlite' || 'hanadb',
+        'db.namespace': 'db',
         'db.connection_string': s => s === ':memory:' || s.match(/jdbc/),
         // 'db.statement':
         //   'SELECT json_insert(\'{}\',\'$."createdAt"\',createdAt,\'$."createdBy"\',createdBy,\'$."modifiedAt"\',modifiedAt,\'$."modifiedBy"\',modifiedBy,\'$."ID"\',ID,\'$."title"\',title,\'$."descr"\',descr,\'$."author_ID"\',author_ID,\'$."genre_ID"\',genre_ID,\'$."stock"\',stock,\'$."price"\',price,\'$."currency_code"\',currency_code) as _json_ FROM (SELECT "$B".createdAt,"$B".createdBy,"$B".modifiedAt,"$B".modifiedBy,"$B".ID,"$B".title,"$B".descr,"$B".author_ID,"$B".genre_ID,"$B".stock,"$B".price,"$B".currency_code FROM sap_capire_bookshop_Books as "$B" WHERE "$B".title is not ?)',
-        'db.statement': s => s.match(/SELECT/),
-        'db.operation': 'READ',
+        'db.query.text': s => s.match(/SELECT/),
+        'db.operation.name': 'READ',
         'db.sql.table': 'sap.capire.bookshop.Books'
       }
     },
@@ -19,14 +19,14 @@ module.exports = {
       // name: "@cap-js/sqlite - stmt.all SELECT json_insert('{}','$.\"createdAt\"',createdAt,'$.…",
       name: s => s.match(/@cap-js\/\w+ - stmt\.all /) || s.match(/@cap-js\/\w+ - exec /),
       attributes: {
-        'code.function': s => s === 'all' || 'exec',
-        'db.system': s => s === 'sqlite' || 'hanadb',
-        'db.name': 'db',
+        'code.function.name': s => s === 'all' || 'exec',
+        'db.system.name': s => s === 'sqlite' || 'hanadb',
+        'db.namespace': 'db',
         'db.connection_string': s => s === ':memory:' || s.match(/jdbc/),
         // 'db.statement':
         //   'SELECT json_insert(\'{}\',\'$."createdAt"\',createdAt,\'$."createdBy"\',createdBy,\'$."modifiedAt"\',modifiedAt,\'$."modifiedBy"\',modifiedBy,\'$."ID"\',ID,\'$."title"\',title,\'$."descr"\',descr,\'$."author_ID"\',author_ID,\'$."genre_ID"\',genre_ID,\'$."stock"\',stock,\'$."price"\',price,\'$."currency_code"\',currency_code) as _json_ FROM (SELECT "$B".createdAt,"$B".createdBy,"$B".modifiedAt,"$B".modifiedBy,"$B".ID,"$B".title,"$B".descr,"$B".author_ID,"$B".genre_ID,"$B".stock,"$B".price,"$B".currency_code FROM sap_capire_bookshop_Books as "$B" WHERE "$B".title is not ?)',
-        'db.statement': s => s.match(/SELECT/),
-        'db.operation': 'READ',
+        'db.query.text': s => s.match(/SELECT/),
+        'db.operation.name': 'READ',
         'db.sql.table': 'sap.capire.bookshop.Books',
         'db.client.response.returned_rows': 5
       }
