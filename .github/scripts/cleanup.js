b74e9f9614d84ed9f9a7b8227b6c3b98bf46c64c
D050513 <sebastian.van.syckel@sap.com>
2025-07-14 11:09:56 +0200

    cleanup

--- .github/scripts/cleanup.js
+++ .github/scripts/cleanup.js
@@ -6,12 +6,14 @@ const headers = { authorization: `Bearer ${token}`, 'content-type': 'application
 const url = 'https://service-manager.cfapps.eu10.hana.ondemand.com'
 
 const contains = 'telemetry'
+const AGE = 1000 * 60 * 60 * 1 // 1 hour
 
 const b_url = url + `/v1/service_bindings?fieldQuery=name contains '${contains}'`
 const b_res = await fetch(b_url, { method: 'GET', headers })
-const { items: bindings } = await b_res.json()
+let { items: bindings } = await b_res.json()
+bindings = bindings.filter(b => Date.now() - new Date(b.created_at) > AGE)
 
-console.log(`Found ${bindings.length} service bindings containing '${contains}'`)
+console.log(`>>> Found ${bindings.length} old service bindings containing '${contains}'`)
 
 for (const binding of bindings) {
   await fetch(url + `/v1/service_bindings/${binding.id}`, { method: 'DELETE', headers })
@@ -19,9 +21,10 @@ for (const binding of bindings) {
 
 const i_url = url + `/v1/service_instances?fieldQuery=name contains '${contains}'`
 const i_res = await fetch(i_url, { method: 'GET', headers })
-const { items: instances } = await i_res.json()
+let { items: instances } = await i_res.json()
+instances = instances.filter(i => Date.now() - new Date(i.created_at) > AGE)
 
-console.log(`Found ${instances.length} service instances containing '${contains}'`)
+console.log(`>>> Found ${instances.length} old service instances containing '${contains}'`)
 
 for (const instance of instances) {
   await fetch(url + `/v1/service_instances/${instance.id}`, { method: 'DELETE', headers })
