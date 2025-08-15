bc56e3a158a1fe9a260dfd38dbf41ba67d660366
D050513 <sebastian.van.syckel@sap.com>
2025-08-15 12:10:39 +0200

    patch once

--- test/passport.test.js
+++ test/passport.test.js
@@ -2,25 +2,22 @@ process.env.SAP_PASSPORT = 'true'
 
 const cds = require('@sap/cds')
 const { expect, GET } = cds.test().in(__dirname + '/bookshop')
-// const log = cds.test.log()
 
 describe('SAP Passport', () => {
   if (cds.env.requires.db.kind === 'sqlite') return test.skip('n/a for SQLite', () => {})
 
-  let _passports, _count
+  const admin = { auth: { username: 'alice' } }
 
+  let _patched
   cds.on('connect', srv => {
-    if (srv.options.kind === 'hana') {
+    if (srv.options.kind === 'hana' && !_patched) {
+      _patched = true
       const { acquire } = srv
       srv.acquire = async function () {
         const dbc = await acquire.apply(this, arguments)
-        const {
-          _native: { set }
-        } = dbc
-        console.warn('>>> PATCH SET')
+        const { set } = dbc._native
         dbc._native.set = function (obj) {
           if ('SAP_PASSPORT' in obj) {
-            console.info('SAP_PASSPORT:', obj.SAP_PASSPORT)
             _passports.push(obj.SAP_PASSPORT)
             _count++
           }
@@ -31,9 +28,7 @@ describe('SAP Passport', () => {
     }
   })
 
-  const admin = { auth: { username: 'alice' } }
-
-  // beforeEach(log.clear)
+  let _passports, _count
   beforeEach(() => {
     _passports = []
     _count = 0
@@ -51,19 +46,6 @@ describe('SAP Passport', () => {
   })
 
   test('gets set twice for prepared statements', async () => {
-    const { status } = await GET("/odata/v4/admin/Books?$filter=title eq 'hurz'", admin)
-    expect(status).to.equal(200)
-    console.info('_count:', _count)
-    console.info('_passports:', _passports)
-    // expect(_passports).to.equal([])
-    expect(_count).to.equal(3)
-    expect(_passports[0]).to.equal('') //> the reset
-    expect(_passports[1]).to.match(/^2A54482A/)
-    expect(_passports[2]).to.match(/^2A54482A/)
-    expect(_passports[1]).to.not.equal(_passports[2]) //> different for prepare and execute
-  })
-
-  test('again', async () => {
     const { status } = await GET("/odata/v4/admin/Books?$filter=title eq 'hurz'", admin)
     expect(status).to.equal(200)
     console.info('_count:', _count)
