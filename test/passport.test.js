eff537741e3500423afbda45f2e49e0e399d0d9e
D050513 <sebastian.van.syckel@sap.com>
2025-08-15 10:21:43 +0200

    test

--- test/passport.test.js
+++ test/passport.test.js
@@ -5,16 +5,24 @@ const { expect, GET } = cds.test().in(__dirname + '/bookshop')
 const log = cds.test.log()
 
 describe('SAP Passport', () => {
-  let _session_vars, _count
+  if (cds.env.requires.db.kind === 'sqlite') return test.skip('n/a for SQLite', () => {})
+
+  let _passports, _count
 
   cds.on('connect', srv => {
-    if (srv.options.kind === 'sqlite') {
+    if (srv.options.kind === 'hana') {
       const { acquire } = srv
       srv.acquire = async function () {
         const dbc = await acquire.apply(this, arguments)
-        dbc.set ??= o => {
-          _session_vars = Object.assign(_session_vars || {}, o)
-          _count++
+        const {
+          _native: { set }
+        } = dbc
+        dbc._native.set = function (obj) {
+          if ('SAP_PASSPORT' in obj) {
+            _passports.push(obj.SAP_PASSPORT)
+            _count++
+          }
+          return set.apply(this, arguments)
         }
         return dbc
       }
@@ -25,14 +33,25 @@ describe('SAP Passport', () => {
 
   beforeEach(log.clear)
   beforeEach(() => {
-    _session_vars = undefined
+    _passports = []
     _count = 0
   })
 
-  test('gets set', async () => {
-    const { status } = await GET('/odata/v4/admin/Books', admin)
+  test('gets set for simple queries', async () => {
+    const { status } = await GET('/odata/v4/catalog/Books', admin)
     expect(status).to.equal(200)
-    expect(_session_vars).to.containSubset({ SAP_PASSPORT: s => s.match(/^2A54482A/) })
     expect(_count).to.equal(2)
+    expect(_passports[0]).to.equal('') //> the reset
+    expect(_passports[1]).to.match(/^2A54482A/)
+  })
+
+  test('gets set for prepared statements', async () => {
+    const { status } = await GET("/odata/v4/catalog/Books?$filter=title eq 'hurz'", admin)
+    expect(status).to.equal(200)
+    expect(_count).to.equal(3)
+    expect(_passports[0]).to.equal('') //> the reset
+    expect(_passports[1]).to.match(/^2A54482A/)
+    expect(_passports[2]).to.match(/^2A54482A/)
+    expect(_passports[1]).to.not.equal(_passports[2]) //> different for prepare and execute
   })
 })
