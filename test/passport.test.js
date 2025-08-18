d958804db17e67a24fef16eddcf504d0b0baefda
sjvans <30337871+sjvans@users.noreply.github.com>
2025-08-18 15:44:25 +0200

    fix: SAP Passport with `_hana_prom = true` (#369)

--- test/passport.test.js
+++ test/passport.test.js
@@ -2,37 +2,54 @@ process.env.SAP_PASSPORT = 'true'
 
 const cds = require('@sap/cds')
 const { expect, GET } = cds.test().in(__dirname + '/bookshop')
-const log = cds.test.log()
 
 describe('SAP Passport', () => {
-  let _session_vars, _count
+  if (cds.env.requires.db.kind === 'sqlite') return test.skip('n/a for SQLite', () => {})
 
-  cds.on('connect', srv => {
-    if (srv.options.kind === 'sqlite') {
-      const { acquire } = srv
-      srv.acquire = async function () {
+  const admin = { auth: { username: 'alice' } }
+
+  cds.on('connect', async service => {
+    if (service.kind === 'hana') {
+      const { acquire } = service
+      service.acquire = async function () {
         const dbc = await acquire.apply(this, arguments)
-        dbc.set ??= o => {
-          _session_vars = Object.assign(_session_vars || {}, o)
-          _count++
+        if (!dbc._native.set._patched) {
+          const { set } = dbc._native
+          dbc._native.set = function (obj) {
+            if ('SAP_PASSPORT' in obj) {
+              _passports.push(obj.SAP_PASSPORT)
+              _count++
+            }
+            return set.apply(this, arguments)
+          }
+          dbc._native.set._patched = true
         }
         return dbc
       }
     }
   })
 
-  const admin = { auth: { username: 'alice' } }
-
-  beforeEach(log.clear)
+  let _passports, _count
   beforeEach(() => {
-    _session_vars = undefined
+    _passports = []
     _count = 0
   })
 
-  test('gets set', async () => {
-    const { status } = await GET('/odata/v4/admin/Books', admin)
+  test('gets set once for simple queries', async () => {
+    const { status } = await GET('/odata/v4/admin/Books?$select=ID,title', admin)
     expect(status).to.equal(200)
-    expect(_session_vars).to.containSubset({ SAP_PASSPORT: s => s.match(/^2A54482A/) })
     expect(_count).to.equal(2)
+    expect(_passports[0]).to.equal('') //> the reset
+    expect(_passports[1]).to.match(/^2A54482A/)
+  })
+
+  test('gets set twice for prepared statements', async () => {
+    const { status } = await GET("/odata/v4/admin/Books?$select=ID,title&$filter=title eq 'hurz'", admin)
+    expect(status).to.equal(200)
+    expect(_count).to.equal(3)
+    expect(_passports[0]).to.equal('') //> the reset
+    expect(_passports[1]).to.match(/^2A54482A/)
+    expect(_passports[2]).to.match(/^2A54482A/)
+    expect(_passports[1]).to.not.equal(_passports[2]) //> different for prepare and execute
   })
 })
