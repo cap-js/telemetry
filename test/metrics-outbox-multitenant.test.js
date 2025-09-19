c567f66ceee080e836ac03711e0ba427edcff509
Paul <paul.erlenwein@gmail.com>
2025-09-19 15:50:33 +0200

    fix: isolation for metrics tests (#377)
    Co-authored-by: sjvans <30337871+sjvans@users.noreply.github.com>

--- test/metrics-outbox-multitenant.test.js
+++ test/metrics-outbox-multitenant.test.js
@@ -1,6 +1,6 @@
 process.env.cds_requires_outbox = true
 process.env.cds_requires_telemetry_metrics = JSON.stringify({
-  config: { exportIntervalMillis: 100 },
+  config: { exportIntervalMillis: 250 },
   _db_pool: false,
   _queue: true,
   exporter: {
@@ -43,7 +43,6 @@ describe('queue metrics for multi tenant service', () => {
     [T2]: { auth: { username: `user_${T2}` } }
   }
 
-  let totalCold = { [T1]: 0, [T2]: 0 }
   let totalInc = { [T1]: 0, [T2]: 0 }
   let totalOut = { [T1]: 0, [T2]: 0 }
 
@@ -58,6 +57,9 @@ describe('queue metrics for multi tenant service', () => {
       return req.reply('OK')
     })
 
+    // Register handler to avoid error due to unhandled action
+    unboxedService.on('call', req => req.reply('OK'))
+
     unboxedService.before('*', () => {
       totalOut[cds.context.tenant] += 1
     })
@@ -67,22 +69,13 @@ describe('queue metrics for multi tenant service', () => {
     await mts.subscribe(T2)
   })
 
-  beforeEach(() => (consoleDirLogs.length = 0))
+  beforeEach(async () => {
+    await cds.tx({ tenant: T1 }, () => DELETE.from('cds.outbox.Messages'))
+    await cds.tx({ tenant: T2 }, () => DELETE.from('cds.outbox.Messages'))
+    consoleDirLogs.length = 0
+  })
 
   describe('given the target service succeeds immediately', () => {
-    let unboxedService
-
-    beforeAll(async () => {
-      unboxedService = await cds.connect.to('ExternalService')
-
-      unboxedService.on('call', req => {
-        return req.reply('OK')
-      })
-    })
-
-    afterAll(async () => {
-      unboxedService.handlers.before = unboxedService.handlers.before.filter(handler => handler.on !== 'call')
-    })
     test('metrics are collected per tenant', async () => {
       if (cds.version.split('.')[0] < 9) return
 
@@ -91,9 +84,9 @@ describe('queue metrics for multi tenant service', () => {
         GET('/odata/v4/proxy/proxyCallToExternalService', user[T2])
       ])
 
-      await wait(150) // Wait for metrics to be collected
+      await wait(300) // Wait for metrics to be collected
 
-      expect(metricValue(T1, 'cold_entries')).to.eq(totalCold[T1])
+      expect(metricValue(T1, 'cold_entries')).to.eq(0)
       expect(metricValue(T1, 'incoming_messages')).to.eq(totalInc[T1])
       expect(metricValue(T1, 'outgoing_messages')).to.eq(totalOut[T1])
       expect(metricValue(T1, 'remaining_entries')).to.eq(0)
@@ -101,7 +94,7 @@ describe('queue metrics for multi tenant service', () => {
       expect(metricValue(T1, 'med_storage_time_in_seconds')).to.eq(0)
       expect(metricValue(T1, 'max_storage_time_in_seconds')).to.eq(0)
 
-      expect(metricValue(T2, 'cold_entries')).to.eq(totalCold[T2])
+      expect(metricValue(T2, 'cold_entries')).to.eq(0)
       expect(metricValue(T2, 'incoming_messages')).to.eq(totalInc[T2])
       expect(metricValue(T2, 'outgoing_messages')).to.eq(totalOut[T2])
       expect(metricValue(T2, 'remaining_entries')).to.eq(0)
@@ -114,8 +107,7 @@ describe('queue metrics for multi tenant service', () => {
   describe('given a target service that requires retries', () => {
     if (cds.version.split('.')[0] < 9) return
 
-    let currentRetryCount = { [T1]: 0, [T2]: 0 }
-    let unboxedService
+    let currentRetryCount, unboxedService
 
     beforeAll(async () => {
       unboxedService = await cds.connect.to('ExternalService')
@@ -129,6 +121,10 @@ describe('queue metrics for multi tenant service', () => {
       unboxedService.handlers.before = unboxedService.handlers.before.filter(handler => handler.before !== 'call')
     })
 
+    beforeEach(() => {
+      currentRetryCount = { [T1]: 0, [T2]: 0 }
+    })
+
     test('storage time increases before message can be delivered', async () => {
       if (cds.version.split('.')[0] < 9) return
 
@@ -138,42 +134,18 @@ describe('queue metrics for multi tenant service', () => {
         GET('/odata/v4/proxy/proxyCallToExternalService', user[T2])
       ])
 
-      await wait(150) // ... for metrics to be collected
-      expect(currentRetryCount[T1]).to.eq(1)
-      expect(currentRetryCount[T2]).to.eq(1)
-
-      expect(metricValue(T1, 'cold_entries')).to.eq(totalCold[T1])
-      expect(metricValue(T1, 'incoming_messages')).to.eq(totalInc[T1])
-      expect(metricValue(T1, 'outgoing_messages')).to.eq(totalOut[T1])
-      expect(metricValue(T1, 'remaining_entries')).to.eq(1)
-      expect(metricValue(T1, 'min_storage_time_in_seconds')).to.eq(0)
-      expect(metricValue(T1, 'med_storage_time_in_seconds')).to.eq(0)
-      expect(metricValue(T1, 'max_storage_time_in_seconds')).to.eq(0)
-
-      expect(metricValue(T2, 'cold_entries')).to.eq(totalCold[T2])
-      expect(metricValue(T2, 'incoming_messages')).to.eq(totalInc[T2])
-      expect(metricValue(T2, 'outgoing_messages')).to.eq(totalOut[T2])
-      expect(metricValue(T2, 'remaining_entries')).to.eq(1)
-      expect(metricValue(T2, 'min_storage_time_in_seconds')).to.eq(0)
-      expect(metricValue(T2, 'med_storage_time_in_seconds')).to.eq(0)
-      expect(metricValue(T2, 'max_storage_time_in_seconds')).to.eq(0)
-
-      // Wait for the first retry to be initiated
-      while (currentRetryCount[T1] < 2) await wait(100)
-      while (currentRetryCount[T2] < 2) await wait(100)
-      await wait(150) // ... for the retry to be processed and metrics to be collected
-      expect(currentRetryCount[T1]).to.eq(2)
-      expect(currentRetryCount[T2]).to.eq(2)
-
+      // Wait for the first retry to be processed
+      while (currentRetryCount[T1] < 2) await wait(10)
+      while (currentRetryCount[T2] < 2) await wait(10)
+    
       // Wait until at least 1 second has passed since the initial call
       const timeAfterFirstRetry = Date.now()
       if (timeAfterFirstRetry - timeOfInitialCall < 1000) {
         await wait(1000 - (timeAfterFirstRetry - timeOfInitialCall))
       }
+      await wait(300) // ... for metrics to be collected
 
-      await wait(200) // ... for metrics to be collected again
-
-      expect(metricValue(T1, 'cold_entries')).to.eq(totalCold[T1])
+      expect(metricValue(T1, 'cold_entries')).to.eq(0)
       expect(metricValue(T1, 'incoming_messages')).to.eq(totalInc[T1])
       expect(metricValue(T1, 'outgoing_messages')).to.eq(totalOut[T1])
       expect(metricValue(T1, 'remaining_entries')).to.eq(1)
@@ -181,7 +153,7 @@ describe('queue metrics for multi tenant service', () => {
       expect(metricValue(T1, 'med_storage_time_in_seconds')).to.be.gte(1)
       expect(metricValue(T1, 'max_storage_time_in_seconds')).to.be.gte(1)
 
-      expect(metricValue(T2, 'cold_entries')).to.eq(totalCold[T2])
+      expect(metricValue(T2, 'cold_entries')).to.eq(0)
       expect(metricValue(T2, 'incoming_messages')).to.eq(totalInc[T2])
       expect(metricValue(T2, 'outgoing_messages')).to.eq(totalOut[T2])
       expect(metricValue(T2, 'remaining_entries')).to.eq(1)
@@ -189,14 +161,12 @@ describe('queue metrics for multi tenant service', () => {
       expect(metricValue(T2, 'med_storage_time_in_seconds')).to.be.gte(1)
       expect(metricValue(T2, 'max_storage_time_in_seconds')).to.be.gte(1)
 
-      // Wait for the second retry to be initiated
-      while (currentRetryCount[T1] < 3) await wait(100)
-      while (currentRetryCount[T2] < 3) await wait(100)
-      await wait(150) // ... for the retry to be processed and metrics to be collected
-      expect(currentRetryCount[T1]).to.eq(3)
-      expect(currentRetryCount[T2]).to.eq(3)
+      // Wait for the second retry to be processd
+      while (currentRetryCount[T1] < 3) await wait(10)
+      while (currentRetryCount[T2] < 3) await wait(10)
+      await wait(600) // ... for metrics to be collected
 
-      expect(metricValue(T1, 'cold_entries')).to.eq(totalCold[T1])
+      expect(metricValue(T1, 'cold_entries')).to.eq(0)
       expect(metricValue(T1, 'incoming_messages')).to.eq(totalInc[T1])
       expect(metricValue(T1, 'outgoing_messages')).to.eq(totalOut[T1])
       expect(metricValue(T1, 'remaining_entries')).to.eq(0)
@@ -204,7 +174,7 @@ describe('queue metrics for multi tenant service', () => {
       expect(metricValue(T1, 'med_storage_time_in_seconds')).to.eq(0)
       expect(metricValue(T1, 'max_storage_time_in_seconds')).to.eq(0)
 
-      expect(metricValue(T2, 'cold_entries')).to.eq(totalCold[T2])
+      expect(metricValue(T2, 'cold_entries')).to.eq(0)
       expect(metricValue(T2, 'incoming_messages')).to.eq(totalInc[T2])
       expect(metricValue(T2, 'outgoing_messages')).to.eq(totalOut[T2])
       expect(metricValue(T2, 'remaining_entries')).to.eq(0)
@@ -217,11 +187,13 @@ describe('queue metrics for multi tenant service', () => {
   describe('given a taget service that fails unrecoverably', () => {
     let unboxedService
 
+    const didProcess = { [T1]: false, [T2]: false }
+
     beforeAll(async () => {
       unboxedService = await cds.connect.to('ExternalService')
 
-      unboxedService.before('call', req => {
-        totalCold[cds.context.tenant] += 1
+      unboxedService.before('call', req =>  {
+        didProcess[cds.context.tenant] = true
         return req.reject({ status: 418, unrecoverable: true })
       })
     })
@@ -238,23 +210,19 @@ describe('queue metrics for multi tenant service', () => {
         GET('/odata/v4/proxy/proxyCallToExternalService', user[T2])
       ])
 
-      await wait(150) // ... for metrics to be collected
+      while (!didProcess[T1]) await wait(10)
+      while (!didProcess[T2]) await wait(10)
+      await wait(500) // ... for metrics to be collected
 
-      expect(metricValue(T1, 'cold_entries')).to.eq(totalCold[T1])
+      expect(metricValue(T1, 'cold_entries')).to.eq(1)
       expect(metricValue(T1, 'incoming_messages')).to.eq(totalInc[T1])
       expect(metricValue(T1, 'outgoing_messages')).to.eq(totalOut[T1])
       expect(metricValue(T1, 'remaining_entries')).to.eq(0)
-      expect(metricValue(T1, 'min_storage_time_in_seconds')).to.eq(0)
-      expect(metricValue(T1, 'med_storage_time_in_seconds')).to.eq(0)
-      expect(metricValue(T1, 'max_storage_time_in_seconds')).to.eq(0)
 
-      expect(metricValue(T2, 'cold_entries')).to.eq(totalCold[T2])
+      expect(metricValue(T2, 'cold_entries')).to.eq(1)
       expect(metricValue(T2, 'incoming_messages')).to.eq(totalInc[T2])
       expect(metricValue(T2, 'outgoing_messages')).to.eq(totalOut[T2])
       expect(metricValue(T2, 'remaining_entries')).to.eq(0)
-      expect(metricValue(T2, 'min_storage_time_in_seconds')).to.eq(0)
-      expect(metricValue(T2, 'med_storage_time_in_seconds')).to.eq(0)
-      expect(metricValue(T2, 'max_storage_time_in_seconds')).to.eq(0)
     })
   })
 })
