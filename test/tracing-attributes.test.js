ff61df055225ebd10d87b30728f74667f6207c58
D051920 <v.kozyura@sap.com>
2026-05-21 11:26:55 +0200

    Instrumental hook approach

--- test/tracing-attributes.test.js
+++ test/tracing-attributes.test.js
@@ -2,12 +2,48 @@ process.env.cds_requires_telemetry_tracing_exporter_module = '@opentelemetry/sdk
 
 const cds = require('@sap/cds')
 const { expect, data } = cds.test().in(__dirname + '/bookshop')
+const http = require('http')
 
 describe('tracing attributes', () => {
   beforeEach(data.reset)
 
   const log = jest.spyOn(console, 'dir')
   beforeEach(log.mockClear)
+
+  describe('remote', () => {
+    let server, port
+
+    beforeAll(done => {
+      server = http.createServer((req, res) => {
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify({ value: [] }))
+      })
+      server.listen(0, () => {
+        port = server.address().port
+        done()
+      })
+    })
+
+    afterAll(done => {
+      server.close(done)
+    })
+
+    test('HTTP client attributes are set on remote service span', async () => {
+      // configure destination URL directly on credentials
+      cds.env.requires.TestRemote = { kind: 'odata', credentials: { url: `http://localhost:${port}` } }
+      const remote = await cds.connect.to('TestRemote')
+
+      // no mock handler - let it make the actual HTTP call
+      await remote.send({ method: 'GET', path: '/test' })
+
+      const output = JSON.stringify(log.mock.calls)
+      expect(output).to.match(/"http\.request\.method":"GET"/)
+      expect(output).to.match(/"http\.response\.status_code":200/)
+      expect(output).to.match(new RegExp(`"url\\.full":"http://localhost:${port}/test"`))
+      expect(output).to.match(/"server\.address":"localhost"/)
+      expect(output).to.match(new RegExp(`"server\\.port":${port}`))
+    })
+  })
 
   describe('db', () => {
     const _db_spans = require('./_db_spans')
