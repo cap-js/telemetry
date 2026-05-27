d2a03cb23a2708690d9ed030d87b026c1dfff08b
D050513 <sebastian.van.syckel@sap.com>
2026-05-27 10:25:34 +0200

    cleanup

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,4 @@ if (process.env.CI && process.env.HANA_DRIVER) {
     process.env.cds_requires_telemetry_tracing = JSON.stringify({ _hana_prom: process.env.HANA_PROM === 'true' })
 }
 
-// use node-native sqlite driver to avoid postinstall scripts
-if (!process.env.HANA_DRIVER) process.env.CDS_REQUIRES_DB_DRIVER = 'node'
-
 module.exports = config
