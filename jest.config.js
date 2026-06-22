9a36e8f024a2dc299f0421e6e0a8a2111332d55f
D050513 <sebastian.van.syckel@sap.com>
2026-06-22 23:26:55 +0200

    oxfmt

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,9 @@ if (process.env.CI && process.env.HANA_DRIVER) {
   config.testMatch = ['**/tracing-attributes.test.js', '**/passport.test.js']
 
   if (process.env.HANA_PROM)
-    process.env.cds_requires_telemetry_tracing = JSON.stringify({ _hana_prom: process.env.HANA_PROM === 'true' })
+    process.env.cds_requires_telemetry_tracing = JSON.stringify({
+_hana_prom: process.env.HANA_PROM === 'true'
+    })
 }
 
 module.exports = config
