247dc625af5091dff8fedcc5380f8fdfdbb11140
D050513 <sebastian.van.syckel@sap.com>
2026-06-26 17:09:33 +0200

    re-add remote tests

--- test/tracing-attributes.test.js
+++ test/tracing-attributes.test.js
@@ -1,11 +1,50 @@
 const cds = require('@sap/cds')
 const { expect, data } = cds.test(__dirname + '/bookshop', '--profile', 'tracing-attributes')
+const http = require('http')
 
 describe('tracing attributes', () => {
   beforeEach(data.reset)
 
   const log = jest.spyOn(console, 'dir')
   beforeEach(log.mockClear)
+
+  describe('remote', () => {
+    let server, port
+
+    beforeAll(done => {
+      server = http.createServer((req, res) => {
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify({ value: [] }))
+      })
+      server.listen(0, () => {
+        port = server.address().port
+        done()
+      })
+    })
+
+    afterAll(done => {
+      server.close(done)
+    })
+
+    test('HTTP client attributes are set on remote service span', async () => {
+      // skip for cds 8 due to Cloud SDK resilience module resolution issues in test environment
+      if (Number(cds.version.split('.')[0]) < 9) return
+
+      // configure destination URL directly on credentials
+      cds.env.requires.TestRemote = { kind: 'odata', credentials: { url: `http://localhost:${port}` } }
+      const remote = await cds.connect.to('TestRemote')
+
+      // no mock handler - let it make the actual HTTP call
+      await remote.send({ method: 'GET', path: '/test' })
+
+      const output = JSON.stringify(log.mock.calls)
+      expect(output).to.match(/"http\.request\.method":"GET"/)
+      expect(output).to.match(/"http\.response\.status_code":200/)
+      expect(output).to.match(new RegExp(`"url\\.full":"http://localhost:${port}/test"`))
+      expect(output).to.match(/"server\.address":"localhost"/)
+      expect(output).to.match(new RegExp(`"server\\.port":${port}`))
+    })
+  })
 
   describe('db', () => {
     const _db_spans = require('./_db_spans')
